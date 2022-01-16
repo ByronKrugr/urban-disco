c087bf414c6ffcd9cf3ef87109717a62df40a8c3
Byron Kruger <bkruger@jhb.dvt.co.za>
2022-01-16 22:06:42 +0200

    mdified model service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   value = 'World';
 
   constructor(private http: HttpClient) {
+    console.log("IN_MODEL_SERVICE")
     this.http.get('/api/message').subscribe((res: any) => {
       console.log("HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO")
       this.value = res.text;
