73b8672898b29efba9fd81475e4a85bd63c6af4a
Byron Kruger <bkruger@jhb.dvt.co.za>
2022-01-16 21:33:59 +0200

    Add API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   value = 'World';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message').subscribe(res => {
+      console.log("HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO")
+    })
+  }
 }
